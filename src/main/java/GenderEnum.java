97268efc4ec17dff3d270a88478ff0552273b088
Filip M <filip.moszczynski@onet.pl>
2020-08-09 11:31:48 +0200

    First working connection formatted - looking good

---
+++ src/main/java/GenderEnum.java
@@ -0,0 +1,12 @@
+public enum GenderEnum {
+    M("mężczyzna"),F("kobieta");
+
+    String name;
+
+    GenderEnum(String fullName) {
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
-import java.math.BigDecimal;
 import java.sql.*;
 
 public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello JDBC :)");
-//
-//         }
 
 
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc_company?serverTimezone=Europe/Warsaw";
@@ -15,18 +10,19 @@ public class Main {
     public static void main(String[] args) throws SQLException{
         Connection connection= DriverManager.getConnection(URL, USER, PASSWORD);
         Statement statement= connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ORDER BY position");
         while(resultSet.next()) {
-            //first_name, last_name, gender, position, salary, id
             String title= resultSet.getString(5);
             String firstName = resultSet.getString(1);
             String lastName = resultSet.getString(2);
             String gender = resultSet.getString(3);
             String salary = resultSet.getString(4);
-            System.out.print(String.format("Stanowisko: %s; ", title));
-            System.out.print(String.format("Imię i Nazwisko: %s %s; ", firstName, lastName));
+            String id = resultSet.getString(6);
+            System.out.print(String.format("ID: %3s; ", id));
+            System.out.print(String.format("Stanowisko: %-24s; ", title));
+            System.out.print(String.format("Imię i Nazwisko: %-10s %-16s; ", firstName, lastName));
             System.out.print(String.format("Płeć: %s; ", gender));
-            System.out.println(String.format("Wysokość wynagrodzenia: %s; ", salary));
+            System.out.println(String.format("Wysokość wynagrodzenia: %5s; ", salary));
         }
         statement.close();
         connection.close();
